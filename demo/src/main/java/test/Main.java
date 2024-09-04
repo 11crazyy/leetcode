c526271530000c0f430307fbe520baaec457496e
crazy2005 <2470345682@qq.com>
2024-09-04 09:33:22 +0800

    双指针

--- demo/src/main/java/test/Main.java
+++ demo/src/main/java/test/Main.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        IsSubsequence isSubsequence = new IsSubsequence();
-        isSubsequence.isSubsequence("b", "abc");
+        TwoSum twoSum = new TwoSum();
+        int[] res = twoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
     }
 }
