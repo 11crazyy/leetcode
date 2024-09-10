e73da344423a8527bebf03ca32ed26ec3d75166a
crazy2005 <2470345682@qq.com>
2024-09-10 19:20:28 +0800

    岛屿问题

---
+++ demo/src/main/java/test/isLandPerimeter.java
@@ -0,0 +1,33 @@
+package test;
+
+public class isLandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    return dfs(grid, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int dfs(int[][] grid, int r, int c) {
+        if (!inArea(grid, r, c)) {
+            return 1;
+        }
+        if (grid[r][c] == 0) {//海洋
+            return 1;
+        }
+        if (grid[r][c] != 1) {
+            return 0;
+        }
+        grid[r][c] = 2;
+        return dfs(grid, r + 1, c) + dfs(grid, r - 1, c) + dfs(grid, r, c + 1) +
+                dfs(grid, r, c - 1);
+    }
+
+    public boolean inArea(int[][] grid, int r, int c) {
+        return r >= 0 && r < grid.length && c >= 0 && c < grid[0].length;
+    }
+}
