3a2d90a49a01a0d6395512a4c3e36386ddc590ca
crazy2005 <2470345682@qq.com>
2024-09-12 09:36:21 +0800

    背包问题

--- demo/src/main/java/test/NumSquares.java
+++ demo/src/main/java/test/NumSquares.java
@@ -1,7 +1,19 @@
 package test;
 
+import java.util.Arrays;
+
 public class NumSquares {
     public int numSquares(int n) {
-
+        //dp[j] = Math.min(dp[j],dp[j-i*i] + 1);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i * i <= n; i++) {
+            int x = i * i;
+            for (int j = x; j <= n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - x] + 1);
+            }
+        }
+        return dp[n];
     }
 }
