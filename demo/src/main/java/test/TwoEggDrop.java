272f11ecf7bdd6d6844ff23b50ecbcbfe380b424
crazy2005 <2470345682@qq.com>
2024-10-14 16:36:56 +0800

    动态规划

--- demo/src/main/java/test/TwoEggDrop.java
+++ demo/src/main/java/test/TwoEggDrop.java
@@ -1,22 +1,26 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoEggDrop {
     private static final int[] mem = new int[1001];
 
     public int twoEggDrop(int n) {
-        if (n == 0 ){
+        if (n == 0) {
             return 0;
         }
-        if (mem[n] > 0){
+        if (mem[n] > 0) {
             return mem[n];
         }
         int res = Integer.MAX_VALUE;
         for (int i = 1; i <= n; i++) {
-            res = Math.min(res,Math.max(i,twoEggDrop(n-i)+1));
+            res = Math.min(res, Math.max(i, twoEggDrop(n - i) + 1));
         }
         mem[n] = res;
         return res;
     }
+
     public int peakIndexInMountainArray(int[] arr) {
         int l = 0, r = arr.length - 1;
         while (l < r) {
@@ -35,6 +39,37 @@ public class TwoEggDrop {
             }
         }
         return l;  // `l` and `r` converge to the peak index
+    }
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] f = new int[target + 1];
+        f[0] = 0;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    f[i] += f[i - num];
+                }
+            }
+        }
+        return f[target];
+    }
+
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        int[] f = new int[high + 1];
+        f[0] = 1;
+        int res = 0;
+        for (int i = 1; i <= high; i++) {
+            if (i - zero >= 0) {
+                f[i] = f[i - zero];
+            }
+            if (i - one >= 0) {
+                f[i] = (f[i] + f[i - one]) % 1000000007;
+            }
+            if (i >= low) {
+                res = (res + f[i]) % 1000000007;
+            }
+        }
+        return res;
     }
 
 }
