bb457ac0ab9acf3e293e4feb55e78246b296279c
crazy2005 <2470345682@qq.com>
2024-10-25 13:32:21 +0800

    每日一题

--- demo/src/main/java/test/Dppp.java
+++ demo/src/main/java/test/Dppp.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,13 +23,36 @@ public class Dppp {
     public int findWinningPlayer(int[] skills, int k) {
         int max = 0;
         int win = 0;
-        for(int i = 1;i < skills.length && win < k;i++){
-            if(skills[i]>skills[max]){
+        for (int i = 1; i < skills.length && win < k; i++) {
+            if (skills[i] > skills[max]) {
                 max = i;
                 win = 0;
             }
             win++;
         }
         return max;
+    }
+
+    public int maxTotalReward(int[] rewardValues) {
+        //f[j] = f[j] || f[j-v]表示能不能得到奖励数j
+        Arrays.sort(rewardValues);
+        int sum = 0;
+        for(int rewardValue : rewardValues){
+            sum += rewardValue;
+        }
+        boolean[] dp = new boolean[2*rewardValues[rewardValues.length-1]];
+        dp[0] = true;
+        for(int rewardValue : rewardValues){
+            for(int j = 2*rewardValue-1;j >= rewardValue;j--){
+                dp[j] = dp[j] || dp[j-rewardValue];
+            }
+        }
+        int res = 0;
+        for(int i = 0;i < dp.length;i++){
+            if(dp[i]){
+                res = i;
+            }
+        }
+        return res;
     }
 }
